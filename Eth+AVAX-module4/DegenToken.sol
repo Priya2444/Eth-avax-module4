470e8441fdea97856074182ce73e3f71316028f8
Priya2444 <137447104+Priya2444@users.noreply.github.com>
2023-08-10 10:19:03 +0530

    DegenToken.sol

--- Eth+AVAX-module4/DegenToken.sol
+++ Eth+AVAX-module4/DegenToken.sol
@@ -10,20 +10,39 @@ contract DegenToken is ERC20, Ownable, ERC20Burnable {
 
     constructor() ERC20("Degen", "DGN") {}
 
-        function mint(address to, uint256 amount) public onlyOwner{
-            _mint(to, amount);
-        }
-        function transferTokens(address _reciever, uint amount) external{
-            require(balanceOf(msg.sender) >= amount, "you are not owner");
-            approve(msg.sender, amount);
-            transferFrom(msg.sender, _reciever, amount);
-        }
-        function checkBalance() external view returns(uint){
-           return balanceOf(msg.sender);
-        }
-        function burnTokens(uint amount) external{
-            require(balanceOf(msg.sender)>= amount, "You do not have enough Tokens");
-            _burn(msg.sender, amount);
-        }
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function transferTokens(address _receiver, uint256 amount) external {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        approve(msg.sender, amount);
+        transferFrom(msg.sender, _receiver, amount);
+    }
+
+    function checkBalance() external view returns (uint) {
+        return balanceOf(msg.sender);
+    }
+
+    function burnTokens(uint256 amount) external {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _burn(msg.sender, amount);
+    }
+
+   
+    function redeemPrize(uint256 prizeCost) external {
+        require(balanceOf(msg.sender) >= prizeCost, "Insufficient balance for prize");
+        
+        
+        _burn(msg.sender, prizeCost);
+        
+        
+        emit PrizeRedeemed(msg.sender, prizeCost);
+        
+        console.log("Prize redeemed by address:", msg.sender, "Prize cost:", prizeCost);
+    }
+
+   
+    event PrizeRedeemed(address indexed user, uint256 prizeCost);
 }
 
